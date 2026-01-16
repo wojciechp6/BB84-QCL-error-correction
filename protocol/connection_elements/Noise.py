8f852e44055c5bf8c1570738feb88d77b19c4de1
wpalka-icg <wpalka@icecodegames.com>
2026-01-16 10:53:18 +0100

    noise experiment

--- protocol/connection_elements/Noise.py
+++ protocol/connection_elements/Noise.py
@@ -7,29 +7,12 @@ from protocol.connection_elements.ConnectionElement import ConnectionElement
 
 
 class Noise(ConnectionElement):
-    """
-    Kanał tożsamościowy z szumem.
-    Można wybrać typ szumu: depolarizing, phase.
-    """
-
-    _index = 0
-    _gates = ["id"]
-    _operation_map = {
-        "id": lambda qc: qc.id(0),
-    }
-    def __init__(self, quantum_error: QuantumError | List[QuantumError], is_new_circuit: bool = False):
-        if is_new_circuit:
-            Noise._index = 0
-        self.gate = Noise._gates[Noise._index]
+    def __init__(self, quantum_error: QuantumError, name: str="Noise"):
         self.error = quantum_error
-        if not isinstance(self.error, list):
-            self.error = [self.error]
-        Noise._index = Noise._index + 1
+        self.name = name
 
     def qc(self, channel: QuantumRegister, i: int, ctx: dict) -> QuantumCircuit:
-        if self.error is not None:
-            for error in self.error:
-                ctx['noise_model'].add_all_qubit_quantum_error(error, [self.gate], warnings=True)
-        qc = QuantumCircuit(1)
-        Noise._operation_map[self.gate](qc)
+        qc = QuantumCircuit(channel, name=self.name)
+        for qbit in channel:
+            qc.append(self.error.to_instruction(), [qbit])
         return qc
