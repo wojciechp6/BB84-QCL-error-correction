2708c611d7fb5521ecf01d68667c740f565b5c33
wpalka-icg <wpalka@icecodegames.com>
2026-01-23 13:02:30 +0100

    3 qubit test

--- protocol/connection_elements/error_correction/ThreeQubitCorrection.py
+++ protocol/connection_elements/error_correction/ThreeQubitCorrection.py
@@ -1,4 +1,4 @@
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 
 from protocol.connection_elements.ConnectionElement import ConnectionElement
 
@@ -23,12 +23,13 @@ class ThreeQubitBitFlipDecoder(ConnectionElement):
         super().__init__()
         self.name = name
         self.anscilla = QuantumRegister(2, name="anscilla")
+        self.test = ClassicalRegister(2, name="testr")
 
     def init(self, n_bits: int, channel_size:int=1, seed=None):
         assert channel_size == 3, "3 Qubit Error Correction requires a channel size of 3"
 
     def qc(self, channel: QuantumRegister, i: int|None, ctx: dict) -> QuantumCircuit:
-        qc = QuantumCircuit(channel, self.anscilla, name=self.name)
+        qc = QuantumCircuit(channel, self.anscilla, self.test, name=self.name)
         qc.cx(channel[0], self.anscilla[0])
         qc.cx(channel[1], self.anscilla[0])
         qc.cx(channel[0], self.anscilla[1])
@@ -41,8 +42,13 @@ class ThreeQubitBitFlipDecoder(ConnectionElement):
         qc.x(self.anscilla[1])
         qc.ccx(self.anscilla[0], self.anscilla[1], channel[2])
         qc.x(self.anscilla[0])
+        qc.measure(self.anscilla, self.test)
+        # qc.measure(self.anscilla[1], self.test[1])
         return qc
 
     def qregs(self) -> list:
         return [self.anscilla]
+
+    def cregs(self) -> list:
+        return [self.test]
 
